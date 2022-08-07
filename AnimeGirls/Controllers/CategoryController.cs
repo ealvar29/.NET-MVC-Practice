2feb576d20c9373be3553d7aea6a4a66dc7404cb
Eduardo <ealvarez@carminaticonsulting.com>
2022-08-07 18:45:38 -0500

    making category controller

--- AnimeGirls/Controllers/CategoryController.cs
+++ AnimeGirls/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnimeGirls.Data;
+using AnimeGirls.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace AnimeGirls.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objectList = _db.Category;
+            return View(objectList);
         }
     }
 }
